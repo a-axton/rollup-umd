a6a2faa43300db34fffe486b6f0b42c82f8aaf78
a-axton <dizzysfarm@gmail.com>
2017-04-21 15:33:36 -0700

    added eslint, edconfig

--- dist/bundle.js
+++
@@ -1,1 +0,0 @@
-!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";console.log("foo")});
--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,13 @@ import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
+import eslint from 'rollup-plugin-eslint';
 import { minify } from 'uglify-js';
 
 const isProd = process.env.PROD;
 const moduleName = require('./package.json').config.moduleName;
 const plugins = [
+  eslint(),
   json(),
   babel({ exclude: 'node_modules/**' }),
   nodeResolve({ jsnext: true, main: true }),
@@ -23,6 +25,6 @@ export default {
   format: 'umd',
   moduleName,
   plugins,
-  dest: 'dist/bundle.js',
+  dest: `dist/${moduleName}.js`,
   sourceMap: !isProd
 };
